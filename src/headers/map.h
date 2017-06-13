23741d51fe14afa138a62e9c84dca72813100d3d
John Agapeyev <jagapeyev@gmail.com>
2017-06-12 17:26:01 -0700

    Added static assert to tt

--- src/headers/map.h
+++ src/headers/map.h
@@ -3,6 +3,7 @@
 
 template<typename Key, typename Value, size_t maxSize, typename Hash = std::hash<Key>>
 class CacheMap {
+    static_assert(maxSize > 0, "Size must be greater than zero");
     std::vector<std::vector<std::pair<Value, size_t>>> internalArray{maxSize};
     Hash hashEngine{};
     
